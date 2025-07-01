6f0bda29239c825e3ef71556f32dce1b5b4752a8
LeonLav77 <leonlav77@gmail.com>
2025-07-01 08:35:19 +0200

    Revert vercel

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { vitePlugin as remix } from "@remix-run/dev";
+import { installGlobals } from "@remix-run/node";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
-import { vercelPreset, installGlobals } from '@vercel/remix/vite';
+import { vercelPreset } from '@vercel/remix/vite';
 
 installGlobals({ nativeFetch: true });
 
